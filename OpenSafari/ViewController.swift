90b429ff119c0ae8a78ed44d7330f2c514093d14
Nylon <diego.gomes87@gmail.com>
2016-02-27 10:56:48 -0300

    safara service,nunca renomear o arquivo com o mesmo nome do framework,dá conflito,acredite!

---
+++ OpenSafari/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  OpenSafari
+//
+//  Created by Diego Gomes on 27/02/2016.
+//  Copyright © 2016 Nylon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func btnSafari(sender: AnyObject) {
+        
+        
+        
+    }
+
+}
+
